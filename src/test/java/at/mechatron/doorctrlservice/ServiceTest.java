fb9d20aacfeb4315df5b863c1eede60e3e238c69
David Leonhartsberger <leozilla@gmail.com>
2019-09-27 15:28:14 +0200

    Added junit

--- src/test/java/at/mechatron/doorctrlservice/ServiceTest.java
+++ src/test/java/at/mechatron/doorctrlservice/ServiceTest.java
@@ -1,4 +1,17 @@
 package at.mechatron.doorctrlservice;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class ServiceTest {
+
+    @BeforeAll
+    public void beforeAll() {
+
+    }
+
+    @Test
+    public void singlePersonInView() {
+
+    }
 }
